f92c9993212d278bb2cbc6661c03baab3e92277e
karpov232 <karpov.t.1@mail.ru>
2025-07-06 18:35:06 +0400

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+#import moduls
 import asyncio
 from aiogram import Bot, Dispatcher
 
 from app.handlers import router
 
-
+#The main function checks the bot for unread messages from the user.
 async def main():
-    bot = Bot(token='7840065320:AAF5vd8rIAMLg8SMdsjyL-71XW1oK_8cs2Y')
+    bot = Bot(token='your bots token')
     dp = Dispatcher()
     dp.include_routers(router)
     await dp.start_polling(bot)
 
-
+#we check whether the file main is startup, if so, we run the main function, if not, we do nothing.
 if __name__ == '__main__':
     try:
         asyncio.run(main())
