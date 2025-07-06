cfe106acf6c389b1aba8209cd8da372c3ea8e97d
karpov232 <karpov.t.1@mail.ru>
2025-07-06 19:33:44 +0400

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -9,10 +9,10 @@ choice = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='no'),  #writing the
                              resize_keyboard=True, #changing the keyboard size
                              input_field_placeholder='') #choose what will be displayed on the line when the user enters the text
 
-
+#The difference between these buttons is that they do not send messages to a chat with a bot.
 catalog = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='кепка', callback_data='cap')],
-    [InlineKeyboardButton(text='шорты', callback_data='shorts')],
-    [InlineKeyboardButton(text='футболка', callback_data='t-shirt')]
+    [InlineKeyboardButton(text='what the user sees', callback_data='what the program sends')],
+    [InlineKeyboardButton(text='shorts', callback_data='shorts')],
+    [InlineKeyboardButton(text='123', callback_data='t-shirt')]
     ]
 )
